0635c24c0bcf13cf2a6d50101ade79e47fb50fa6
Robert Thompson <rthomps7@nd.edu>
2014-05-12 22:19:18 -0400

    Initial commit

---
+++ lib/api/node_modules/crud/node_modules/utools/README.md
@@ -0,0 +1,4 @@
+UTOOLS
+======
+
+Install with `npm install crud@git+ssh://git@github.com:uhray/crud.git`.
---
+++ server.js
@@ -0,0 +1,6 @@
+var requirejs = require('requirejs'),
+    reqjs_cfg = require('./lib/config/requirejs.js'),
+    logging = require('./lib/config/logging.js');
+
+requirejs.config({
+});
