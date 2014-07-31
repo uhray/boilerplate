22026491589519d4af8c5b34e1505393a3cd9b45
Robert Thompson <rthomps7@nd.edu>
2014-07-31 00:15:07 -0400

    changing to cookie sessions

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ app.engine('jade', require('jade').__express);
 app.use(require('connect-timeout')(5000));
 app.use(require('morgan')('dev'));
 app.use(require('cookie-parser')());
-app.use(require('express-session')({
+app.use(require('connect-session')({
   secret: '__SECRET__',
-  resave: true,
-  saveUninitialized: true
 }));
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('body-parser').json());
