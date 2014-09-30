417f45b5860c6a8e51a4a0e8deef2d4f430d2925
Nick Schappler <nickschappler@gmail.com>
2014-09-30 10:55:34 -0400

    updating gulpfile to handle underlying foreman execution

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,42 @@
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass');
+    sass = require('gulp-ruby-sass'),
+    child = require('child_process');
 
-gulp.task('express', function() {
-  return require('./server');
+// Top Level Commands ----------------------------------------------------------
+gulp.task('default', ['info']);
+gulp.task('static', ['static-server']);
+gulp.task('dev', ['dev-server', 'dev-scss', 'dev-watch']);
+gulp.task('prod', ['prod-server']);
+
+// Helper Tasks ----------------------------------------------------------------
+
+gulp.task('info', function() {
+  console.log('\nUsage:\t gulp [ static | dev | prod ]\n');
 });
 
-gulp.task('sass', function() {
-  return gulp.src('app/frontend/styles/sass/*.scss')
+gulp.task('static-server', function() {
+  child.spawn('foreman', ['start', 'static'], { stdio: 'inherit' });
+});
+
+gulp.task('dev-server', function() {
+  child.spawn('foreman', ['start', 'dev'], { stdio: 'inherit' });
+});
+
+gulp.task('prod-server', function() {
+  child.spawn('foreman', ['start', 'web'], { stdio: 'inherit' });
+});
+
+gulp.task('dev-scss', function() {
+  return gulp.src('app/frontend/styles/*.scss')
              .pipe(sass({
                 sourcemap: true,
-                sourcemapPath: '../sass'
+                sourcemapPath: '..'
               }))
              .on('error', function (err) { console.log(err.message); })
              .pipe(gulp.dest('app/frontend/styles/css'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('app/frontend/styles/sass/*.scss', ['sass']);
+gulp.task('dev-watch', function() {
+  gulp.watch('app/frontend/styles/*.scss', ['sass']);
 });
 
-
-gulp.task('default', ['express', 'sass', 'watch']);
