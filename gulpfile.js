1affffcc8346f81070de5f58b14a1b38ff2d262e
Nick Schappler <nickschappler@gmail.com>
2014-09-30 13:23:24 -0400

    adding install and post-installs to gulpfile, removing from package.json

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,43 @@ var gulp = require('gulp'),
 
 // Top Level Commands ----------------------------------------------------------
 gulp.task('default', ['info']);
-gulp.task('static', ['static-server', 'dev-scss', 'dev-watch']);
-gulp.task('dev', ['dev-server', 'dev-scss', 'dev-watch']);
-gulp.task('prod', ['prod-server']);
+gulp.task('install', ['npm_install', 'post_install']);
+gulp.task('static', ['install', 'static_server', 'scss_to_css', 'watch_scss']);
+gulp.task('dev', ['install', 'dev_server', 'scss_to_css', 'watch_scss']);
+gulp.task('prod', ['install', 'prod_server']);
 
 // Helper Tasks ----------------------------------------------------------------
 
 gulp.task('info', function() {
-  console.log('\nUsage:\t gulp [ static | dev | prod ]\n');
+  console.log('\nUsage:\t gulp [ install | static | dev | prod ]\n');
 });
 
-gulp.task('static-server', function() {
+gulp.task('npm_install', function() {
+  child.spawn('npm', ['install'], { stdio: 'inherit' });
+});
+
+gulp.task('post_install', function() {
+  child.spawn('./node_modules/bower/bin/bower', ['cache', 'clean'],
+              { stdio: 'inherit' });
+  child.spawn('./node_modules/bower/bin/bower', ['install'],
+              { stdio: 'inherit' });
+  child.spawn('./node_modules/requirejs/bin/r.js', ['-o', 'config/rjs-build.js'],
+              { stdio: 'inherit' });
+});
+
+gulp.task('static_server', function() {
   child.spawn('foreman', ['start', 'static'], { stdio: 'inherit' });
 });
 
-gulp.task('dev-server', function() {
+gulp.task('dev_server', function() {
   child.spawn('foreman', ['start', 'dev'], { stdio: 'inherit' });
 });
 
-gulp.task('prod-server', function() {
+gulp.task('prod_server', function() {
   child.spawn('foreman', ['start', 'web'], { stdio: 'inherit' });
 });
 
-gulp.task('dev-scss', function() {
+gulp.task('scss_to_css', function() {
   return gulp.src('app/frontend/styles/*.scss')
              .pipe(sass({
                 sourcemap: true,
@@ -36,7 +50,7 @@ gulp.task('dev-scss', function() {
              .pipe(gulp.dest('app/frontend/styles/css'));
 });
 
-gulp.task('dev-watch', function() {
-  gulp.watch('app/frontend/styles/*.scss', ['dev-scss']);
+gulp.task('watch_scss', function() {
+  gulp.watch('app/frontend/styles/*.scss', ['scss_to_css']);
 });
 
