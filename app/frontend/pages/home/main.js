a6a3dca09c8b81b8cc0bdcc7c9bbc7234807bd10
Robert Thompson <rthomps7@nd.edu>
2015-01-29 10:59:00 -0500

    change home file syntax

--- app/frontend/pages/home/main.js
+++ app/frontend/pages/home/main.js
@@ -6,20 +6,22 @@ define(
 function(Ractive, template) {
 
   return function() {
-    var ractive = new Ractive({
-          el: '#body',
-          template: template,
-          data: {
-            time: new Date(),
-            showModal: false
-          },
-          computed: {
-            timestamp: function() {
-              var t = this.get('time');
-              return t.getHours() + ':' + t.getMinutes() + ':' + t.getSeconds();
-            }
-          }
-        });
+    var ractive;
+
+    ractive = new Ractive({
+      el: '#body',
+      template: template,
+      data: {
+        time: new Date(),
+        showModal: false
+      },
+      computed: {
+        timestamp: function() {
+          var t = this.get('time');
+          return t.getHours() + ':' + t.getMinutes() + ':' + t.getSeconds();
+        }
+      }
+    });
 
     ractive.on('modal.teardown', function() {
       this.set('showModal', false);
