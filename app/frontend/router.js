2761731c5b6cd31b4fa1a8e0cd40797f3f34201b
Robert Thompson <rthomps7@nd.edu>
2015-02-11 13:22:32 -0500

    Simplify router

--- app/frontend/router.js
+++ app/frontend/router.js
@@ -1,26 +1,13 @@
 define(
 [
 'director',
-'loader!pages',
-'debug'
+'loader!pages'
 ],
-function(Director, pages, debug) {
-  var debug = debug('router'),
-      routes = {
+function(Director, pages) {
+  var routes = {
         '/': pages.home
       },
       router = new Director(routes);
 
-  router.configure({
-    before: clear
-  });
-
   router.init('/');
-
-  function clear() {
-    debug('clearing for new page');
-    var b = document && document.getElementById &&
-            document.getElementById('body');
-    if (b && 'innerHTML' in b) b.innerHTML = '';
-  }
 });
